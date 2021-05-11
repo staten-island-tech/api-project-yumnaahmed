e65e43666777b69824204fbdbc13ed9a17d85b52
yumna65 <71685041+yumna65@users.noreply.github.com>
2021-05-10 22:30:26 -0400

    beginning javascript
    don't forget to insert API from postman

---
+++ js/index.js
@@ -0,0 +1,8 @@
+const query = async function () {
+  try {
+      const response= await fetch(//insert API key here)
+  } catch (error) {
+    console.log(error);
+    alert("Hey something went wrong");
+  }
+};
