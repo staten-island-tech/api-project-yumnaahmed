e9f5b0be7efd1837925f72467d64e345de69085c
yumna65 <71685041+yumna65@users.noreply.github.com>
2021-05-27 02:11:58 -0400

    bruh

--- js/index.js
+++ js/index.js
@@ -3,25 +3,26 @@
 const query = async function () {
   try {
     const response = await fetch(
-      `https://api.sunrise-sunset.org/json?${lat}${lng}`
+      `https://api.sunrise-sunset.org/json?${lat&lng}`
     );
-    // const data = await response.json();
-    // data.results.array.forEach((location) => {
-    //   let locationArr= [];
-    //   const addlocation = function () {
-    //   //   location.forEach((element) => {
-    //   //     if(DeviceMotionEvent.genre_ids.includes(element.id))
-    //   //   })
-
-    //   // }
-    //   DOMSelectors.contentArea.innerHTML = "";
-    //   const latitude = data.data[0].latitude;
-    //   const longitude = data.data[0].longitude;
-    //   console.log(latitude, longitude);
-    // });
-  } catch (error) {
-    console.log(error);
-    alert("Hey, try new coordinates");
   }
-};
-query();
+//     // const data = await response.json();
+//     // data.results.array.forEach((location) => {
+//     //   let locationArr= [];
+//     //   const addlocation = function () {
+//     //   //   location.forEach((element) => {
+//     //   //     if(DeviceMotionEvent.genre_ids.includes(element.id))
+//     //   //   })
+
+//     //   // }
+//     //   DOMSelectors.contentArea.innerHTML = "";
+//     //   const latitude = data.data[0].latitude;
+//     //   const longitude = data.data[0].longitude;
+//     //   console.log(latitude, longitude);
+//     // });
+//   } catch (error) {
+//     console.log(error);
+//     alert("Hey, try new coordinates");
+//   }
+ };
+// query();
