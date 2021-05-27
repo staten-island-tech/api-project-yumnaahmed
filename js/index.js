71c9f3929a557e81ffec1091c46b84ef28dd424d
mr. whalen <michael.whalen@sitechhs.com>
2021-05-27 12:38:34 -0400

    Whalen

--- js/index.js
+++ js/index.js
@@ -1,27 +1,27 @@
-import { DOMSelectors } from ".DOM";
+import { DOMSelectors } from "DOM";
 let location = "lat,lng";
 const query = async function () {
   try {
+    const response = await fetch(
+      `https://api.sunrise-sunset.org/json?lat=90&lng=45`
+    );
+    const data = await response.json();
+    console.log(data);
   } catch (error) {
     console.log(error);
     alert("Hey, try new coordinates");
   }
-  const response = await fetch(
-    `https://api.sunrise-sunset.org/json?${lat}${lng}`
-  );
 };
-const data = await response.json();
-console.log(data);
-data.results.forEach((location) => {
+
+/* data.results.forEach((location) => {
   console.log(location);
-});
-// const displayData = function (data) {
+}) */ // const displayData = function (data) {
 //   DOMSelectors.contentArea.innerHTML = "";
 //   const latitude= data.data[0].lat_location;
 //   const longitude= data.data[0].lng_location;
 //   console.log(lat,lng);
 
-data.results.forEach((location) => {
+/* data.results.forEach((location) => {
   DOMSelectors.grid.insertAdjacentHTML(
     "beforeend",
     `<div class="location">
@@ -29,5 +29,5 @@ data.results.forEach((location) => {
       <h4 class="sunset">Written By: ${sunset}</h4>
       `
   );
-});
+}); */
 query();
