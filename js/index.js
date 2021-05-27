28cf8fd866634b83a04428bb2c4a0ef92889788c
yumna65 <71685041+yumna65@users.noreply.github.com>
2021-05-27 00:17:09 -0400

    why do i do this

--- js/index.js
+++ js/index.js
@@ -5,23 +5,23 @@ const query = async function () {
     const response = await fetch(
       `https://api.sunrise-sunset.org/json?${lat}${lng}`
     );
-    const data = await response.json();
-    data.results.array.forEach((location) => {
-      let locationArr= [];
-      const addlocation = function () {
-      //   location.forEach((element) => {
-      //     if(DeviceMotionEvent.genre_ids.includes(element.id))
-      //   })
+    // const data = await response.json();
+    // data.results.array.forEach((location) => {
+    //   let locationArr= [];
+    //   const addlocation = function () {
+    //   //   location.forEach((element) => {
+    //   //     if(DeviceMotionEvent.genre_ids.includes(element.id))
+    //   //   })
 
-      // }
-      DOMSelectors.contentArea.innerHTML = "";
-      const latitude = data.data[0].latitude;
-      const longitude = data.data[0].longitude;
-      console.log(latitude, longitude);
-    });
+    //   // }
+    //   DOMSelectors.contentArea.innerHTML = "";
+    //   const latitude = data.data[0].latitude;
+    //   const longitude = data.data[0].longitude;
+    //   console.log(latitude, longitude);
+    // });
   } catch (error) {
     console.log(error);
     alert("Hey, try new coordinates");
   }
 };
-query;
+query();
