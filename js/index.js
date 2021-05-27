8adb55529b06ed0d98a98d93ad1d721e9aef7c77
yumna65 <71685041+yumna65@users.noreply.github.com>
2021-05-26 23:39:20 -0400

    bruh

--- js/index.js
+++ js/index.js
@@ -1,38 +1,20 @@
 import { DOMSelectors } from "./DOM";
 
-const init = function () {
-  let searchTerm;
-  // const apiKey =(insert key);
-
 const query = async function () {
   try {
     const response = await fetch(
-      `https://api.sunrise-sunset.org/json?${lat&lng}` 
+      `https://api.sunrise-sunset.org/json?${lat}${lng}`
     );
     const data = await response.json();
-    data.results.forEach((charity) => {
-      DOMSelectors.grid.insertAdjacentHTML(
-        "beforeend,/* work on html bro */"
-      ); /*method of the Element interface parses the specified text as HTML and inserts the resulting nodes into the DOM tree at a specified position */
+    data.results.array.forEach((location) => {
+      DOMSelectors.contentArea.innerHTML = "";
+      const latitude = data.data[0].latitude;
+      const longitude = data.data[0].longitude;
+      console.log(latitude, longitude);
     });
   } catch (error) {
     console.log(error);
-    alert("Hey,the sunrise and sunset for this area isn't available");
+    alert("Hey, try new coordinates");
   }
 };
-
-// DOMSelectors.contentArea.innerHTML = "";
-//     const searchTerm = data.data[0].searchTerm_name;
-//     const city = data.data[0].city_code;
-//     const state = data.data[0].state;
-//     console.log(searchTerm, city, state);
-
-    letkeywArr = [];
-    const addKeyword = function (){
-      addKeyword.forEach((element) => {
-        /* if(addKeyword.) */
-      };)
-
-    }
-
-init();
+query;
