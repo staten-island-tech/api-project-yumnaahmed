30e847ce8cf38b2daaf087ad54562667b4a20167
yumna65 <71685041+yumna65@users.noreply.github.com>
2021-05-27 03:41:07 -0400

    utgh

--- js/DOM.js
+++ js/DOM.js
@@ -1,7 +1,7 @@
-const DOMSelectors = {
+/* const DOMSelectors = {
   locationInput: document.getElementById("search-area"),
   submitBtn: document.getElementById("submit-button"),
   contentArea: document.getElementById("search-form"),
 };
 
-export { DOMSelectors };
+export { DOMSelectors }; */
