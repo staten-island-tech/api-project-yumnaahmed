cd61f7be453f9fac742dd03e866062b324dfe6fe
mr. whalen <michael.whalen@sitechhs.com>
2021-06-04 07:29:38 -0400

    fixed

--- js/index.js
+++ js/index.js
@@ -7,7 +7,7 @@ const query = async function () {
       `https://api.sunrise-sunset.org/json?lat=90&lng=50`
     );
     const data = await response.json();
-    console.log(data);
+
     DOMSelectors.sunData.insertAdjacentHTML(
       "beforeend",
       `<div class="location">
@@ -21,21 +21,34 @@ const query = async function () {
   }
 };
 
-/* data.results.forEach((location) => {
-  console.log(location);
-}) */ // const displayData = function (data) {
-//   DOMSelectors.contentArea.innerHTML = "";
-//   const latitude= data.data[0].lat_location;
-//   const longitude= data.data[0].lng_location;
-//   console.log(lat,lng);
-
-/* data.results.forEach((location) => {
-  DOMSelectors.grid.insertAdjacentHTML(
-    "beforeend",
-    `<div class="location">
-      <h3 class="sunrise">${sunrise}</h3>
-      <h4 class="sunset">Written By: ${sunset}</h4>
-      `
-  );
-}); */
 query();
+
+const listen = function () {
+  DOMSelectors.contentArea.addEventListener("submit", function (e) {
+    e.preventDefault();
+    const searchParams = DOMSelectors.locationInput.value;
+    //remove old html here using innerHTML =""", simply figure out where your JS  inserts the HTML and instead of insert, just do innerHTML = ""
+    const query = async function () {
+      try {
+        const response = await fetch(
+          `https://api.sunrise-sunset.org/json?lat=90&lng=${searchParams}`
+        );
+        const data = await response.json();
+        console.log(data);
+        DOMSelectors.sunData.insertAdjacentHTML(
+          "beforeend",
+          `<div class="location">
+            <h3 class="sunrise">${data.results.sunrise}</h3>
+            <h4 class="sunset">Written By: ${data.results.sunset}</h4>
+            `
+        );
+      } catch (error) {
+        console.log(error);
+        alert("Hey, try new coordinates");
+      }
+    };
+
+    query();
+  });
+};
+listen();
